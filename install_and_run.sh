66265c3af1993d0ca77070d97767a0f4c5cfe25b
Sumit Chaturvedi <sumit.chaturvedi@gmail.com>
2023-12-13 19:13:08 +0000

    Added prompt for conda

--- install_and_run.sh
+++ install_and_run.sh
@@ -1,3 +1,7 @@
+echo "Please enter the full path to your Miniconda3 installation:"
+read miniconda_path
+source "$miniconda_path/etc/profile.d/conda.sh"
+
 echo "Creating conda environment (sam). Please make sure you have conda installed ..."
 conda create --name sam --file spec_file.txt
 
@@ -7,6 +11,11 @@ conda activate sam
 echo "Installing some extra dependencies through pip ..."
 python3 -m pip install -r requirements.txt
 python3 -m pip install -e .
+
+echo "Downloading the SAM checkpoint from https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth"
+echo "If this doesn\'t happen automatically, please download checkpoint yourself and put it in checkpoints"
+wget https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth
+mv sam_vit_h_4b8939.pth checkpoints/
 
 echo "Downloading checkpoint from https://drive.google.com/uc?id=1WZc7wen-K2EhwmZaRUebK76nHWtKPlk_"
 echo "If this doesn\'t happen automatically, please download checkpoint through browser and put it in lightning_logs/version_27/checkpoints/"
